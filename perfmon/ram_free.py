15625bd2b7b5042c14237edf2278d7ec4f140832
delmar74 <delmar74@rambler.ru>
2018-10-13 09:56:44 +0300

    Update ram_free.py

--- perfmon/ram_free.py
+++ perfmon/ram_free.py
@@ -1,7 +1,13 @@
+##########################
+# IF FREE_RAM < CRITICAL_VALUE : { ACTION }
+##########################
+
 import os
 
+# Critical value
 critical = 13
 
+# Free memory percentage
 mem=str(os.popen('free | grep Mem | awk \'{print 100*$4/$2}\' ').readlines())
 value = float(mem[2:-4])
 
